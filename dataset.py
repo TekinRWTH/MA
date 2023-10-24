39671d5c12cc0c6bded896306c207aac59bafdfa
TekinRWTH <93874297+TekinRWTH@users.noreply.github.com>
2023-10-24 12:52:51 +0200

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,45 @@
+import numpy as np
+import config
+import os
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.utils import save_image
+
+
+class MapDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.list_files = [f for f in os.listdir(self.root_dir) if f.endswith('.tiff')]
+
+    def __len__(self):
+        return len(self.list_files)
+
+    def __getitem__(self, index):
+        img_file = self.list_files[index]
+        img_path = os.path.join(self.root_dir, img_file)
+        image = np.array(Image.open(img_path))
+        input_image = image[:, :1216, :]
+        target_image = image[:, 1216:, :]
+
+        augmentations = config.both_transform(image=input_image, image0=target_image)
+        input_image = augmentations["image"]
+        target_image = augmentations["image0"]
+
+        input_image = config.transform_only_input(image=input_image)["image"]
+        target_image = config.transform_only_mask(image=target_image)["image"]
+
+        return input_image, target_image
+
+
+if __name__ == "__main__":
+    dataset = MapDataset("/Users/deniztekin/Documents/Programme/Masterarbeit/images/AB")
+    loader = DataLoader(dataset, batch_size=5)
+    for x, y in loader:
+        print(x.shape)
+        save_image(x, "x.tiff")
+        save_image(y, "y.tiff")
+        import sys
+
+        sys.exit()
+
+
