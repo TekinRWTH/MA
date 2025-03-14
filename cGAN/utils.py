4477e4eb0e2ad1d9e287117671555d746ed15d9b
TekinRWTH <93874297+TekinRWTH@users.noreply.github.com>
2025-03-14 16:09:50 +0100

    Create utils.py

---
+++ cGAN/utils.py
@@ -0,0 +1,52 @@
+import torch
+import config
+from torchvision.utils import save_image
+
+
+
+
+
+def save_some_examples(gen, val_loader, epoch, folder):
+    gen.eval()
+    with torch.no_grad():
+        for batch_idx, (x, y) in enumerate(val_loader):  # <-- Changed: Added a loop to iterate over all batches
+            x, y = x.to(config.DEVICE), y.to(config.DEVICE)
+            y_fake = gen(x)
+            y_fake = y_fake * 0.5 + 0.5  # remove normalization
+            save_image(y_fake, folder + f"/y_gen_{epoch}_batch_{batch_idx}.tiff")  # <-- Changed: Added batch index to file name
+            if epoch <= 1:
+                save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}_batch_{batch_idx}.tiff")  # <-- Changed: Added batch index to file name
+            if epoch == 1:
+                save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}_batch_{batch_idx}.tiff")  # <-- Changed: Added batch index to file name        
+            
+            
+    gen.train()
+
+
+def save_checkpoint(model, optimizer, epoch, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+        "epoch": epoch
+    }
+    torch.save(checkpoint, filename)
+
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+   
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+        
+    return checkpoint.get("epoch", 0)
+
+        
+        
+        
+        
+        
